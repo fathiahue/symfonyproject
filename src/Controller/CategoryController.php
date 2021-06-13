b5b5fc7e9802ab31fccf9f305472294838060366
fathiahue <fatgt@hotmail.fr>
2021-06-13 09:18:27 +0200

    correction quête 8

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,26 +2,66 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Category;
+use App\Entity\Program;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
+
+/**
+ * @Route("/categories/", name="category_index")
+ */
 class CategoryController extends AbstractController
 {
-    /**
-     * @Route("/category/", name="category_index")
-     */
-
-     public function index() : Response
+    
+        public function index() : Response
 
      {
-        $programs = $this->getDoctrine()
+        $category = $this->getDoctrine()
             ->getRepository(Category::class)
             ->findAll();
             return $this->render(
                 'category/index.html.twig',
-                ['category' => $category]
+                ['categories' => $categories]
             );
             
 }
+
+
+
+    /**
+     * @Route("/{categoryName}", name="category_show")
+     */
+
+    public function show(string $CategoryName) : Response
+
+    {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(['name' =>$categoryName]);
+
+            if (!$category) {
+                throw $this->createNotFoundException(
+                    'No category with name : '.$categoryName.' found in category\'s table.'
+                );
+                }
+        
+                $programs = $this->getDoctrine()
+                                 ->getRepository(Program::class)
+                                 ->findBy([
+                                     'category' => $category
+                                 ], [
+                                     'id' => 'DESC'
+                                 ], 3);
+        
+            return $this->render('category/show.html.twig', [
+                'category' => $category,
+                'programs' => $programs
+
+                ]);
+                }
+    
+
 }
